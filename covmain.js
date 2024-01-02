bad1d71f54c6b2aebfe3dc5a1a9c794523aedebf
Lia <150208830+LliaLia@users.noreply.github.com>
2024-01-02 23:59:48 +0800

    Add files via upload

---
+++ covmain.js
@@ -0,0 +1,19 @@
+console.log("-------------------------------------")
+console.log("============ PERPUS KAMI ============")
+console.log("====== UAS DDP 2023 - SI PAGI 3 =====")
+console.log("============= KELOMPOK 1 ============")
+console.log("-------------------------------------")
+console.log("")
+
+//function Menu Utama
+function menu (){
+    console.log("--- Menu Utama ---")
+    console.log("1. Tambah Buku")
+    console.log("2. Cari Buku")
+    console.log("3. Hapus Buku")
+    console.log("4. Tambah Peminjaman")
+    console.log("5. Cari Peminjaman")
+    console.log("6. Hapus Peminjaman")
+    console.log("7. Keluar")
+    console.log("")
+}
